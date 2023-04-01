90a854716fb8362869c12c84e6d163c89724dcf7
Emraaaan <emranhasan906@gmail.com>
2023-04-02 00:03:50 +0600

    first commit

---
+++ Module-53/price-list-cards/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    const routes = [
+        {
+          id: 1,
+          name: 'Home',
+          path: '/'
+        },
+        {
+          id: 2,
+          name: 'About',
+          path: '/about'
+        },
+        {
+          id: 3,
+          name: 'Contact',
+          path: '/contact'
+        },
+        {
+          id: 4,
+          name: 'Blog',
+          path: '/blog'
+        },
+        {
+          id: 5,
+          name: 'Post',
+          path: '/blog/:postId'
+        }
+      ]
+      
+    return (
+        <nav>
+            <div onClick={()=>setOpen(!open)} className='md:hidden'>
+                <span>{open == true ? <XMarkIcon className="h-6 w-6 text-purple-500" /> : <Bars3Icon className="h-6 w-6 text-purple-500" />   }</span>
+               
+                
+            </div>
+            <ul className='md:flex'>
+                {
+                    routes.map(route => <Link 
+                        key={route.id}
+                        route={route}
+                        ></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
