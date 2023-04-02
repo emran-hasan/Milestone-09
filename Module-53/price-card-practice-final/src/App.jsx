cbf80630b58603552c3d837069c8d1f868e420db
Emraaaan <emranhasan906@gmail.com>
2023-04-02 21:23:54 +0600

    added

--- Module-53/price-card-practice-final/src/App.jsx
+++ Module-53/price-card-practice-final/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Dashboard from './components/Dashboard/Dashboard'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/PriceList/PriceList'
+import Test from './components/test/test'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar></Navbar>
         <PriceList></PriceList>
         <Dashboard></Dashboard>
+        <Test></Test>
     </div>
   )
 }
